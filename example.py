7f52d932f93e9115401c9df683adff470dfe13a1
Philippe Remy <premy.enseirb@gmail.com>
2020-05-16 07:35:53 +0900

    origin

--- example.py
+++ example.py
@@ -1,8 +1,9 @@
 from time import sleep
 
 from fxrt import fx_rates
+from datetime import datetime
 
 while True:
     prices = fx_rates()
-    print(prices)
+    print(datetime.now().replace(microsecond=0), prices)
     sleep(5)
